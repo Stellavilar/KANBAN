1763b2b3fb5c8726b43404eefc204b6428001b6f
Stella Vilar <vilar.stella@gmail.com>
2020-05-29 16:20:48 +0200

    Multer installed

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-// const multer = require('multer');
+const multer = require('multer');
 
 const router = require ('./BACK/app/router');
 const sanitizeMiddleware = require ('./BACK/app/middleware/sanitise');
@@ -14,6 +14,9 @@ app.use(cors());
 
 //Middleware pour le req.body
 app.use(express.urlencoded({extended: true}));
+// on rajoute multer pour les formulaires au format "multipart"
+const bodyParser = multer();
+app.use( bodyParser.none() );
 
 // ici, req.body existe déjà (grace à urlencoded), et on veut l'assinir AVANT de le passer au router
 app.use( sanitizeMiddleware );
